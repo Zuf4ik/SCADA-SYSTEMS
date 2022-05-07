4847570e90978acf4500768736c16aaf9f0a18c8
Zuf4ik <66068745+Zuf4ik@users.noreply.github.com>
2022-05-07 13:22:53 +0500

    Server

--- server/localserver.js
+++ server/localserver.js
@@ -2,15 +2,48 @@ const { json } = require('express');
 const express = require('express');
 const app = express();
 const WSserver = require('express-ws')(app);
-const fs = require('fs')
+const fs = require('fs');
 
 const PORT = process.env.PORT || 5000;
+const data = require('./dataPos.json');
 
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
 
-app.ws('/', (ws, req) => {
+let i = 0;
+let rndArr = [];
+while ( i < 10 ) {
+    rndArr.push(getRandomInt(50));
+    i++;
+    fs.writeFileSync('signal.json', JSON.stringify(rndArr));
+}
+
+let j = 0;
+let rndJArr = [];
+while ( j < 10 ) {
+    rndJArr.push(getRandomInt(50));
+    j++;
+}
+console.log(rndArr);
+console.log(rndJArr);
+/*while ( i < 10 ) {
+    fs.writeFileSync('signal.json', JSON.stringify(getRandomInt(50)));
+    i++;
+}*/
+i = 0;
+
+app.ws('/', (res, req) => {
     console.log('ПОДКЛЮЧЕНО')
-    ws.send('ты успешно подключился')
-    ws.on('message', (msg) => {
+    /*setTimeout(function() {
+        while ( i < 10) {
+            res.send(getRandomInt(50));
+            i++;
+        }
+    }, 5000)*/
+    res.send(JSON.stringify(rndArr));
+    res.send(JSON.stringify(rndJArr));
+    res.on('message', (msg) => {
         console.log(JSON.parse(msg))
         fs.writeFileSync('dataPos.json', JSON.stringify(msg));
     })
